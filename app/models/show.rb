adaf899f1fb542ad0168a834ed406a8ded8c32fa
shuaib shaban <shuaib>
2023-02-25 11:54:01 +0300

    done test

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
   
-end
+    def actors_list
+      actors.map do |actor|
+        actor.full_name
+      end
+    end
+  end
+  
